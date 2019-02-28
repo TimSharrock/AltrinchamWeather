47d4a665c1d0c9e3d27dff573eae5c8361820786
Tim Sharrock <tim@sharrock.org.uk>
2019-02-28 21:18:50 +0000

    add empty DegreedayCalculator

---
+++ WeatherSolution/WeatherLib/DegreeDayCalculator.cpp
@@ -0,0 +1,7 @@
+﻿#include "pch.h"
+#include "DegreeDayCalculator.h"
+
+double TjsWeather::DegreeDayCalculator::operator()(double aValue, double anotherValue)
+{
+	return 0.0;
+}
---
+++ WeatherSolution/WeatherLib/DegreeDayCalculator.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+namespace TjsWeather
+{
+	class DegreeDayCalculator
+	{
+	public:
+		enum class Direction{Above,Below};
+		DegreeDayCalculator(Direction direction, double threshold) 
+		: myDirection(direction),myThreshold(threshold)
+		{}
+		double operator()(double aValue, double anotherValue);
+	private:
+		Direction myDirection;
+		double myThreshold;
+	};
+}
