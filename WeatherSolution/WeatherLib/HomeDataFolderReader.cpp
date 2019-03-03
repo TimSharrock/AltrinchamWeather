22b5f0bdbd7709dfa382cc85d1f7d1a2b2c49612
Tim Sharrock <tim@sharrock.org.uk>
2019-03-03 16:59:05 +0000

    first bits of file-system awareness, not really intended for the master branch :(

---
+++ WeatherSolution/WeatherLib/HomeDataFolderReader.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "HomeDataFolderReader.h"
+#include <filesystem>
+namespace fs = std::experimental::filesystem;
+
+TjsWeather::HomeDataFolderReader::HomeDataFolderReader()
+{
+	//std::experimental::filesystem::directory_iterator it;
+	 //for(auto& p: std::filesystem::directory_iterator("sandbox"))
+	 {
+		 
+	 }
+}
+
+
+TjsWeather::HomeDataFolderReader::~HomeDataFolderReader()
+{
+}
---
+++ WeatherSolution/WeatherLib/HomeDataFolderReader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace TjsWeather
+{
+	class HomeDataFolderReader
+	{
+	public:
+		HomeDataFolderReader();
+		~HomeDataFolderReader();
+	};
+}
