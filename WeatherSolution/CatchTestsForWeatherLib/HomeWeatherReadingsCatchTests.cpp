38603e461463c77bf2eb4d3d143712be69c0cdc8
Tim Sharrock <tim@sharrock.org.uk>
2019-04-29 23:05:31 +0100

    minor tweaks to date characterisation tests

--- WeatherSolution/CatchTestsForWeatherLib/HomeWeatherReadingsCatchTests.cpp
+++ WeatherSolution/CatchTestsForWeatherLib/HomeWeatherReadingsCatchTests.cpp
@@ -1,6 +1,7 @@
 #include <catch.hpp>
 #include "../WeatherLib/HomeWeatherReadings.h"
 #include <date/date.h>
+using namespace date::literals;
 
 TEST_CASE("C++ language version verification (for <optional>)")
 {
@@ -23,6 +24,11 @@ TEST_CASE("Can create and access HomeWeatherReadings")
       CHECK_FALSE(readings.frontGardenTemperature);
       auto dayPoint = floor<date::days>(readings.timeStamp);
       auto ymd = date::year_month_day{ dayPoint };
-      CHECK(ymd.year() == date::year(1970)); // unix Epoch???
+      CHECK(ymd.year() == 1970_y); // unix Epoch???
+      auto time = date::make_time(readings.timeStamp - dayPoint);
+      CHECK(time.hours().count() == 0);
    }
+
+   //Timestamp; Temperature; Temperature Probe
+   //07.04.2018 15:14:21; 22.1; 22.9
 }
